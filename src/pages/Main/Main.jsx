e28ff51e769535e4fb5f7e896728da6821b096f9
jini <kjhee0394@gmail.com>
2023-09-08 11:57:32 +0900

    Add : 결제 완료페이지  UI 구현 완료

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import NavMain from '../../components/Nav/NavMain';
 import './Main.scss';
 
 export default function Main() {
-  return <div>hi</div>;
+  return (
+    <div className="main">
+      <div>
+        <NavMain />
+      </div>
+    </div>
+  );
 }
