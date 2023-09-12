f63fd49c19fc7e2bcbe724b73f293d2e3504ed56
serenity1091 <seulki1091@gmail.com>
2023-09-12 23:11:10 +0900

    [feat] BuyHistory 구현 완료

--- src/pages/BuyHistory/BuyHistory.jsx
+++ src/pages/BuyHistory/BuyHistory.jsx
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import NavBack from '../../components/Nav/NavBack';
 import './BuyHistory.scss';
 
 export default function BuyHistory() {
+  const [buyData, setBuyData] = useState([]);
+
+  useEffect(() => {
+    const axiosData = async () => {
+      try {
+        const response = await axios.get(
+          'http://10.58.52.129:3000/mypage/buyhistory',
+          {
+            headers: {
+              'Content-Type': 'application/json;charset=utf-8',
+              //authorization: localStorage.getItem('TOKEN'),
+            },
+          },
+        );
+        setBuyData(response.data);
+      } catch (error) {
+        console.error('데이터 불러오기 실패:', error);
+      }
+    };
+
+    axiosData();
+  }, []);
+
   return (
     <div className="buyHistory">
-      <NavBack title="구매내역" />
+      <NavBack title="구매내역" />c
       <div className="buyHistoryList">
-        {/* {buyList.map(buyHistory => ( */}
-        <div className="buyList" key="">
-          <div className="productTitle">
-            <div className="productName">상품명</div>
-            <div className="productPrice">13,000원</div>
-          </div>
-          <div className="paymentBgColor">
-            <div className="payment">
-              <div className="paymentMethod">카드결제</div>
-              <div className="paymentStatus">거래중</div>
+        {buyData.map(buyHistory => (
+          <div className="buyList" key={buyHistory.itemid}>
+            <div className="productTitle">
+              <div className="productName">{buyHistory.title}</div>
+              <div className="productPrice">{buyHistory.price}원</div>
+            </div>
+            <div className="paymentBgColor">
+              <div className="payment">
+                <div className="paymentMethod">카카오페이</div>
+              </div>
             </div>
           </div>
-        </div>
-        {/* ))} */}
+        ))}
       </div>
     </div>
   );
