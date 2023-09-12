b2f29290d9b1b0b40b01d7765757f7adb07772fe
serenity1091 <seulki1091@gmail.com>
2023-09-12 23:29:47 +0900

    [feat] SalesHistory 구현 완료

--- src/pages/SalesHistory/SalesHistory.jsx
+++ src/pages/SalesHistory/SalesHistory.jsx
@@ -1,39 +1,91 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import NavBack from '../../components/Nav/NavBack';
 import './SalesHistory.scss';
 
 export default function SalesHistory() {
+  const [salesData, setSalesData] = useState([]);
   const navigate = useNavigate();
 
   const goToSalesEdit = () => {
     navigate('/sales-edit');
   };
 
+  useEffect(() => {
+    const axiosData = async () => {
+      try {
+        const response = await axios.get(
+          'http://10.58.52.129:3000/mypage/salehistory',
+          {
+            headers: {
+              'Content-Type': 'application/json;charset=utf-8',
+              //authorization: localStorage.getItem('token'),
+            },
+          },
+        );
+        setSalesData(response.data);
+      } catch (error) {
+        console.error('데이터 불러오기 실패:', error);
+      }
+    };
+
+    axiosData();
+  }, []);
+
+  const salesDeleting = item => {
+    const deleteData = async () => {
+      try {
+        const response = await axios.delete('API', {
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+            //authorization: localStorage.getItem('token'),
+          },
+        });
+
+        if (response.status === 200) {
+          alert('삭제 완료');
+        }
+
+        const result = response.data;
+        console.log(result);
+      } catch (error) {
+        console.error('삭제 실패:', error);
+      }
+    };
+
+    deleteData();
+  };
+
   return (
     <div className="salesHistory">
       <NavBack title="판매내역" />
       <div className="salesHistoryList">
-        {/* {salesList.map(buyHistory => ( */}
-        <div className="salesList" key="">
-          <div className="salesProductTitle">
-            <div className="salesProductName">판매 상품명</div>
-            <div className="salesProductPrice">13,000원</div>
-          </div>
-          <div className="salesPaymentBgColor">
-            <div className="sales">
-              <div className="salesEditBtn">
-                <div className="editBtn" onClick={goToSalesEdit}>
-                  수정
+        {salesData.map(salesHistory => (
+          <div className="salesList" key={salesHistory.itemId}>
+            <div className="salesProductTitle">
+              <div className="salesProductName">{salesHistory.title}</div>
+              <div className="salesProductPrice">{salesHistory.price}원</div>
+            </div>
+            <div className="salesPaymentBgColor">
+              <div className="sales">
+                <div className="salesEditBtn">
+                  <div className="editBtn" onClick={goToSalesEdit}>
+                    수정
+                  </div>
+                  <div className="btnDivision" />
+                  <div
+                    className="deleteBtn"
+                    /*onClick={() => salesDeleting(salesHistory.productId)}*/
+                  >
+                    삭제
+                  </div>
                 </div>
-                <div className="btnDivision" />
-                <div className="deleteBtn">삭제</div>
+                <div className="salesStatus">{salesHistory.itemCount}</div>
               </div>
-              <div className="salesStatus">판매중</div>
             </div>
           </div>
-        </div>
-        {/* ))} */}
+        ))}
       </div>
     </div>
   );
