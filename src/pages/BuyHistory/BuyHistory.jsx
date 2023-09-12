ac9ec233aeb81d6987c0cedef4f54bba5234de8c
serenity1091 <seulki1091@gmail.com>
2023-09-12 23:11:10 +0900

    [feat] BuyHistory UI 구현 완료

--- src/pages/BuyHistory/BuyHistory.jsx
+++ src/pages/BuyHistory/BuyHistory.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import NavBack from '../../components/Nav/NavBack';
+import './BuyHistory.scss';
 
-export default function buyHistory() {
-  return <div className="buyHistory"></div>;
+export default function BuyHistory() {
+  return (
+    <div className="buyHistory">
+      <NavBack title="구매내역" />
+      <div className="buyHistoryList">
+        {/* {buyList.map(buyHistory => ( */}
+        <div className="buyList" key="">
+          <div className="productTitle">
+            <div className="productName">상품명</div>
+            <div className="productPrice">13,000원</div>
+          </div>
+          <div className="paymentBgColor">
+            <div className="payment">
+              <div className="paymentMethod">카드결제</div>
+              <div className="paymentStatus">거래중</div>
+            </div>
+          </div>
+        </div>
+        {/* ))} */}
+      </div>
+    </div>
+  );
 }
