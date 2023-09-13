0e5cadc311f6baa371e6a473f4f14cec7b6162cc
serenity1091 <seulki1091@gmail.com>
2023-09-13 19:37:19 +0900

    [feat] Main 소셜 로그인 기능 구현 완료

--- src/pages/Main/components/KakaoCallback.jsx
+++ src/pages/Main/components/KakaoCallback.jsx
@@ -1,41 +1,70 @@
-import { useCallback, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import axios from 'axios';
 import LoggingIn from './LoggingIn';
 
 function KakaoCallback() {
   const navigate = useNavigate();
-  const Address = new URL(window.location.href);
-  const code = Address.searchParams.get('code') || '';
+  const [searchParams] = useSearchParams();
 
-  const fetchLogin = useCallback(
-    async code => {
-      try {
-        const response = await (
-          await fetch('http://localhost:3001/user/kakaologin', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ code }),
-          })
-        ).json();
+  const code = searchParams.get('code') || '';
 
-        console.log(response);
+  const axiosLogin = async code => {
+    try {
+      const tokenData = await getToken(code);
+      const response = await axios.post(
+        'http://localhost:3001/user/kakaosign',
+        {},
+        {
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+            authorization: tokenData.access_token,
+          },
+        },
+      );
+      const result = await response.data;
 
-        navigate('/product-list');
-      } catch (error) {
-        alert('로그인 에러!');
-        console.error(error);
-      }
-    },
-    [navigate],
-  );
+      localStorage.setItem('token', result.accessToken);
+      navigate('/product-list');
+    } catch (error) {
+      alert('로그인 실패. 아이디와 비밀번호를 확인하세요.');
+      console.error(error);
+    }
+  };
+
+  const getToken = async code => {
+    try {
+      const response = await axios.post(
+        'https://kauth.kakao.com/oauth/token',
+        {
+          client_id: process.env.REACT_APP_KAKAO_KEY,
+          code,
+          grant_type: 'authorization_code',
+          redirect_uri: `http://localhost:3000/callback-kakao`,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        },
+      );
+
+      const tokenData = {
+        access_token: response.data.access_token,
+        refresh_token: response.data.refresh_token,
+      };
+
+      return tokenData;
+    } catch (error) {
+      throw new Error('카카오 토큰 가져오기에 실패했습니다');
+    }
+  };
 
   useEffect(() => {
     if (code) {
-      fetchLogin(code);
+      axiosLogin(code);
     }
-  }, [code, fetchLogin]);
+  }, [code, axiosLogin]);
 
   return (
     <div className="callback">
