ef00f331de74d8d6f37f287a6429989fd705c7ad
serenity1091 <seulki1091@gmail.com>
2023-09-14 15:52:27 +0900

    [feat] MyPage 로그아웃 기능 구현 완료

--- src/pages/MyPage/components/LogOut.jsx
+++ src/pages/MyPage/components/LogOut.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function LogOut() {
-  const logOut = () => {};
+  const navigate = useNavigate();
+
+  const logoutKakao = () => {
+    localStorage.removeItem('token');
+    alert('로그아웃 되었습니다.');
+    navigate('/');
+  };
 
   return (
     <>
-      <div onClick={logOut}>로그아웃</div>
+      <div onClick={logoutKakao}>로그아웃</div>
     </>
   );
 }
---
+++ src/pages/ProfileEdit/ProfileEdit.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavBack from '../../components/Nav/NavBack';
+import './ProfileEdit.scss';
+
+export default function ProfileEdit() {
+  return (
+    <div className="profileEdit">
+      <NavBack title="프로필수정" />
+    </div>
+  );
+}
