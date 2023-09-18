6c2b03ca5712906cec166aef87391bb6a8279106
jini <kjhee0394@gmail.com>
2023-09-18 22:44:28 +0900

    [feat] 좌표값 및 카테고리 선택에 따른 상품리스트 호출 기능 구현
    - 현재 내 위치 좌표값 자동 설정 및 쿼리스트링으로 데이터 전달
    - 좌표값 반경 내 상품 호출
    - 카테고리 선택에 따른 상품 호출

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,27 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { GoHeart, GoHeartFill } from 'react-icons/go';
 import './List.scss';
+import axios from 'axios';
 
 const List = ({ list }) => {
+  const navigate = useNavigate();
+  const [like, setLike] = useState(false);
+  const [likeCount, setLikeCount] = useState('');
+  const handleLike = () => {
+    setLike(!like);
+  };
+
+  const handleLikeCount = e => {
+    e.stopPropagation();
+    handleLike();
+    // if (!like) {
+    //   setLikeCount (likeCount +1)
+    //   axios.post("/data/productList.json", {
+    //     userId : userId,
+    //     itemId : itemId
+    //   })
+    // } else if (like) {
+    //   setLikeCount(likeCount -1)
+    //   axios.post("/data/productList.json", {
+    //     userId : userId,
+    //     itemId : itemId
+    //   })
+    // }
+  };
+
   if (!list) {
     return null;
   }
 
   return (
-    <li className="list">
+    <li className="list" onClick={() => navigate(`/product-detail/${list.id}`)}>
       <div className="listImg">
         <img src={list.imgUrl} art={list.title} className="thumbnail" />
-        <div className="likeIcon">
-          <GoHeart />
-          {/* <GoHeartFill className="likeIcon" /> */}
+        <div className="heartButton" onClick={handleLikeCount}>
+          {like ? <GoHeartFill /> : <GoHeart />}
         </div>
       </div>
       <div className="productInfo">
         <p className="productTitle">{list.title}</p>
         <div className="productSubInfo">
-          <p>좋아요 {list.like} | </p>
-          <p className="review">후기 1개</p>
+          <p>좋아요 {list.likeCount} | </p>
+          <p className="review">{list.reviewCount}</p>
         </div>
         <p className="productPrice">{list.price}원</p>
       </div>
