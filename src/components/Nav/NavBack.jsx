bb7b15b62aaa2748e2233be739dd6513a1365878
jini <kjhee0394@gmail.com>
2023-09-19 21:19:57 +0900

    [feat] 내 주변 상품 페이지 뒤로가기 버튼 코드 추가
    - 내 주변 상품 페이지에서 뒤로가기 버튼을 누르면 바로 이동이 되지 않고 쿼리스트링 삭제 후 한 번 더 눌러야 뒤로가는 현상 발생
    - 때문에 uselocation hook으로 현재 페이지 경로를 가져와 경로가 내 주변 상품 페이지일 경우 replace 옵션의 navigate 함수가 실행되어 바로 이전 페이지로 이동되도록 코드 추가

--- src/components/Nav/NavBack.jsx
+++ src/components/Nav/NavBack.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { IoIosArrowBack } from 'react-icons/io';
 import './NavBack.scss';
 
 const NavBack = ({ title }) => {
+  const location = useLocation();
   const navigate = useNavigate();
+
+  const isSurroundingProductPage = location.pathname.includes(
+    '/view-surrounging-products',
+  );
+
   const goToBack = () => {
+    if (isSurroundingProductPage) {
+      navigate(-1, { replace: true });
+    }
     navigate(-1);
   };
 
